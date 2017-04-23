2e458077be66e18f9a97a2d253c9643fa388e63b
Jared Robertson <jared.robertson@usask.ca>
2017-04-22 19:08:12 -0600

    Completed Ch4 and 5 examples

--- rango/views.py
+++ rango/views.py
@@ -2,4 +2,13 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def index(request):
-    return HttpResponse("Rango says hey there world!")
+    # Construct a dictionary to pass to the template engine as its context.
+    context_dict = {'boldmessage': "I am bold font from the context"}
+
+    # Return a rendered response to send to the client.
+    return render(request, 'rango/index.html', context_dict)
+
+
+def about(request):
+    context_dict = {'aboutmessage': "This is the About page context variable"}
+    return render(request, 'rango/about.html', context_dict)
--- tango_with_django_project/settings.py
+++ tango_with_django_project/settings.py
@@ -11,7 +11,9 @@ https://docs.djangoproject.com/en/1.7/ref/settings/
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 import os
 BASE_DIR = os.path.dirname(os.path.dirname(__file__))
-
+TEMPLATE_PATH = os.path.join(BASE_DIR, 'templates')
+STATIC_PATH = os.path.join(BASE_DIR,'static')
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/1.7/howto/deployment/checklist/
@@ -82,3 +84,16 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.7/howto/static-files/
 
 STATIC_URL = '/static/'
+
+STATICFILES_DIRS = (
+    STATIC_PATH,
+)
+
+TEMPLATE_DIRS = (
+    # Put strings here, always using forward slashes
+    # Use absolute paths
+    TEMPLATE_PATH,
+)
+
+MEDIA_URL = '/media/'
+
