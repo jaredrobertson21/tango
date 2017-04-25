fbde7687d4a61786f1b39490db33d208c313c118
Jared Robertson <jared.robertson@usask.ca>
2017-04-24 21:49:02 -0600

    Beginning Chapter 7

--- rango/views.py
+++ rango/views.py
@@ -1,9 +1,10 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from rango.models import Category, Page
 
 def index(request):
-    # Construct a dictionary to pass to the template engine as its context.
-    context_dict = {'boldmessage': "I am bold font from the context"}
+    category_list = Category.objects.order_by('-likes')[:5]
+    context_dict = {'categories': category_list}
 
     # Return a rendered response to send to the client.
     return render(request, 'rango/index.html', context_dict)
@@ -12,3 +13,29 @@ def index(request):
 def about(request):
     context_dict = {'aboutmessage': "This is the About page context variable"}
     return render(request, 'rango/about.html', context_dict)
+
+
+def category(request, category_name_slug):
+    context_dict = {}
+
+    try:
+        # Can we find a category name slug with the given name?
+        # If we can't, the .get() method raises a DoesNotExist exception.
+        # So the .get() method returns one model instance or raises an exception
+        category = Category.objects.get(slug=category_name_slug)
+        context_dict['category_name'] = category.name
+
+        # Retrieve all associated pages
+        # Note that filter returns >= 1 model instance
+        pages = Page.objects.filter(category=category)
+
+        # Adds our results list to the template context under name pages
+        context_dict['pages'] = pages
+
+        # Add category object from the database to the context dictionary
+        # Use this in the template to verify the category exists
+        context_dict['category'] = category
+    except Category.DoesNotExist:
+        pass
+
+    return render(request, 'rango/category.html', context_dict)
